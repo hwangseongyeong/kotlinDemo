8322fdd243dc613900f3007321040d7f295df8d4
황성영 <ghkdtjddud@gmail.com>
2020-05-09 01:20:50 +0900

    2020년 5월 8일 잔디심기

---
+++ src/com/larva/demo/demo20200508/Customer.java
@@ -0,0 +1,9 @@
+package com.larva.demo.demo20200508;
+
+// 자바의 Customer 클래스
+public class Customer {
+    public static final String LEVEL = "BASIC";  // static 필드
+    public static void login() { // static 메서드
+        System.out.println("Login...");
+    }
+}
---
+++ src/com/larva/demo/demo20200508/Test02.kt
@@ -0,0 +1,7 @@
+package com.larva.demo.demo20200508
+
+// 코틀린에서 자바의 static 접근
+fun main() {
+    println(Customer.LEVEL)
+    Customer.login()
+}
